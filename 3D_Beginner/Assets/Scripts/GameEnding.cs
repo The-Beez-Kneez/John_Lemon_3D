c6306b1b3aab49196b6c11d330ce3d59294c5316
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2019-10-27 17:56:12 -0700

    Added Unity 3D project

---
+++ 3D_Beginner/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+  public float fadeDuration =  1f;
+  public float displayImageDuration = 1f;
+  public GameObject player;
+  // End Game by exiting level
+  public CanvasGroup exitBackgroundImageCanvasGroup;
+  // End Game once caught by enemies
+  public CanvasGroup caughtBackgroundImageCanvasGroup;
+
+  bool m_isPlayerAtExit;
+  bool m_isPlayerCaught;
+  float m_Timer;
+
+  void OnTriggerEnter (Collider other) {
+    if(other.gameObject == player) {
+      m_isPlayerAtExit = true;
+    }
+  }
+
+  public void CaughtPlayer () {
+    m_isPlayerCaught = true;
+  }
+
+  void Update () {
+    if(m_isPlayerAtExit) {
+      EndLevel(exitBackgroundImageCanvasGroup, false);
+    } else if (m_isPlayerCaught) {
+      EndLevel(caughtBackgroundImageCanvasGroup, true);
+    }
+  }
+
+  void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart) {
+    m_Timer += Time.deltaTime;
+
+    imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+    if(m_Timer > fadeDuration + displayImageDuration) {
+      if (doRestart) {
+        SceneManager.LoadScene(0);
+      } else {
+        Application.Quit();
+      }
+    }
+  }
+}
---
+++ 3D_Beginner/Assets/Scripts/Observer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+  public Transform player;
+
+  public GameEnding gameEnding;
+
+  bool m_isPlayerInRange;
+
+  void OnTriggerEnter (Collider other) {
+    if (other.transform == player) {
+      m_isPlayerInRange = true;
+    }
+  }
+
+  void OnTriggerExit (Collider other) {
+    if (other.transform == player) {
+      m_isPlayerInRange = false;
+    }
+  }
+
+  // Need to check the line of sight when the player character is actually in range
+  // Chck line of sight by checking whether there are any Colliders along the path of a line starting from a point.
+  void Update () {
+    if (m_isPlayerInRange) {
+      // You may remember that JohnLemon’s position is on the ground, between his feet.  To make sure the Observer can see JohnLemon’s centre of mass, you’re pointing the direction up one unit by adding Vector3.up.  Vector3.up is a shortcut for (0, 1, 0).
+      Vector3 direction = player.position - transform.position + Vector3.up;
+
+      Ray ray = new Ray(transform.position, direction);
+
+      RaycastHit rayCastHit;
+
+      if (Physics.Raycast(ray, out rayCastHit)) {
+        if (rayCastHit.collider.transform == player) {
+          gameEnding.CaughtPlayer ();
+        }
+      }
+    }
+  }
+}
